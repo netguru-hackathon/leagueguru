dc775a9a471c88471364e5839df5ac2a5da51bee
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-08-21 16:10:33 +0200

    Add new form league

--- app/react/index.js
+++ app/react/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import DashboardMain from './dashboard/main.jsx';
+import NewLeagueForm from './components/new-league-form.jsx';
 
 window.React = React;
 window.ReactComponents = {
-  'dashboard-main': DashboardMain
+  'dashboard-main': DashboardMain,
+  'new-league-form': NewLeagueForm
 };
