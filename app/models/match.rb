afa54581eaea3abfd25b3c26a600d807adefae45
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-08-21 14:55:47 +0200

    add relationships to models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,11 @@
 class Match < ActiveRecord::Base
+  belongs_to :league
+  belongs_to :a_user_1, foreign_key: 'a_team_first_id', class: 'User'
+  belongs_to :a_user_2, foreign_key: 'a_team_second_id', class: 'User'
+  belongs_to :b_user_1, foreign_key: 'b_team_first_id', class: 'User'
+  belongs_to :b_user_2, foreign_key: 'b_team_second_id', class: 'User'
+
+  def winner
+    result == true ? [a_user_1, a_user_2] : [b_user_1, b_user_2]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :matches, lambda { |user|
+    where("matches.a_team_first_id=? OR matches.a_team_second_id=? OR \
+           matches.b_team_first_id=? OR matches.b_team_second_id=?", user.id, user.id)
+  }
 end
