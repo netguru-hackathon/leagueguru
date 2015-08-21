61501cfc017fc1cea04841a553bb66a9f2bf1cbe
Piotr Pałek <piotrpalek@gmail.com>
2015-08-21 14:57:07 +0200

    hello component

--- app/react/index.js
+++ app/react/index.js
@@ -1,1 +1,12 @@
 import React from 'react';
+window.React = React;
+
+class HelloComponent extends React.Component {
+    render() {
+      return <div>Hello!!!! Guru</div>;
+    }
+}
+
+window.ReactComponents = {
+  hello: HelloComponent
+};
