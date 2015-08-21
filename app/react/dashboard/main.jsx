3a6376cdcf5f90268672d3e8e4259af58bb8b326
Piotr Pałek <piotrpalek@gmail.com>
2015-08-21 17:05:04 +0200

    list all leagues

--- app/react/dashboard/main.jsx
+++ app/react/dashboard/main.jsx
@@ -3,9 +3,13 @@ import LeaguePanel from './league-panel.jsx';
 
 export default class DashboardMain extends React.Component {
   render() {
+    const leagues = this.props.leagues.map(league => {
+      return <LeaguePanel name={league.name}></LeaguePanel>
+    });
+
     return <div className="row">
       <div className="col-md-offset-3 col-md-6">
-        <LeaguePanel name="Some League"></LeaguePanel>
+        {leagues}
       </div>
     </div>
   }
