8ba410ce1ed2a9bb57d30672c34e73cd0b535d7a
dominik tomczak <dominik.tomczak@netguru.pl>
2015-08-21 17:18:56 +0200

    add omniauth_callbacks_controller + set up routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:google_oauth2]
 
   has_many :matches, lambda { |user|
     where("matches.a_team_first_id=? OR matches.a_team_second_id=? OR \
            matches.b_team_first_id=? OR matches.b_team_second_id=?", user.id, user.id)
   }
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.name = auth.info.first_name
+      user.email = auth.info.email
+      user.password ||= Devise.friendly_token[0,20]
+    end
+  end
 end
