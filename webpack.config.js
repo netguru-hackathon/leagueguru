9b80485b8434c3dac8e7781ab4e8800d2a4dd998
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-08-21 12:06:03 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: __dirname + "/javascript",
+  entry: "./index.js",
+  output: {
+    path: __dirname + "/app/assets/javascripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      },
+    ]
+  },
+  externals: {jquery: 'var jQuery'},
+  plugins: [
+    new ExtractTextPlugin('../stylesheets/bundle.css', {
+      allChunks: true
+    })
+  ]
+};
